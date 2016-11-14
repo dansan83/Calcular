59021db47e95b4ec29045c7f8ea62a4a7503d121
dansan83 <Daniel@MacBook-Pro-de-Daniel.local>
2016-11-14 15:33:42 -0500

    Calculadora

--- app/src/main/java/com/example/daniel/holamundo/MainActivity.java
+++ app/src/main/java/com/example/daniel/holamundo/MainActivity.java
@@ -28,49 +28,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myTextView2 = (TextView) v2;
         myTextView2.setText("");
 
-        final Button button0 = (Button) findViewById(R.id.button0);
+        View button0 = findViewById(R.id.button0);
         button0.setOnClickListener(this);
 
         View button1 = findViewById(R.id.button1);
         button1.setOnClickListener(this);
 
-        final Button button2 = (Button) findViewById(R.id.button2);
+        View button2 = findViewById(R.id.button2);
         button2.setOnClickListener(this);
 
-        final Button button3 = (Button) findViewById(R.id.button3);
+        View button3 = findViewById(R.id.button3);
         button3.setOnClickListener(this);
 
-        final Button button4 = (Button) findViewById(R.id.button4);
+        View button4 = findViewById(R.id.button4);
         button4.setOnClickListener(this);
 
-        final Button button5 = (Button) findViewById(R.id.button5);
+        View button5 = findViewById(R.id.button5);
         button5.setOnClickListener(this);
 
-        final Button button6 = (Button) findViewById(R.id.button6);
+        View button6 = findViewById(R.id.button6) ;
         button6.setOnClickListener(this);
 
-        final Button button7 = (Button) findViewById(R.id.button7);
+        View button7 = findViewById(R.id.button7);
         button7.setOnClickListener(this);
 
-        final Button button8 = (Button) findViewById(R.id.button8);
+        View button8 = findViewById(R.id.button8);
         button8.setOnClickListener(this);
 
-        final Button button9 = (Button) findViewById(R.id.button9);
+        View button9 = findViewById(R.id.button9);
         button9.setOnClickListener(this);
 
-        final Button buttonPunto = (Button) findViewById(R.id.buttonPunto);
+        View buttonPunto = findViewById(R.id.buttonPunto);
         buttonPunto.setOnClickListener(this);
 
-        final Button buttonLimpiar = (Button) findViewById(R.id.buttonLimpiar);
+        View buttonLimpiar = findViewById(R.id.buttonLimpiar);
         buttonLimpiar.setOnClickListener(this);
 
-        final Button buttonSuma = (Button) findViewById(R.id.buttonSuma);
+        View buttonSuma = findViewById(R.id.buttonSuma);
         buttonSuma.setOnClickListener(this);
 
-        final Button buttonResta = (Button) findViewById(R.id.buttonResta);
+        View buttonResta = findViewById(R.id.buttonResta);
         buttonResta.setOnClickListener(this);
 
-        final Button buttonMultiplicar = (Button) findViewById(R.id.buttonMultiplicar);
+        View buttonMultiplicar = findViewById(R.id.buttonMultiplicar);
         buttonMultiplicar.setOnClickListener(this);
 
         final Button buttonIgual = (Button) findViewById(R.id.buttonIgual);
@@ -78,8 +78,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    double result, m1 = 0, m2 = 0;
-    char op1 = '+';
+    double result, numero1 = 0, numero2 = 0;
+    char operacion = '+';
 
     public void onClick(View v) {
         if (v.getId() == R.id.button0)
@@ -107,38 +107,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if (v.getId() == R.id.buttonLimpiar) {
             myTextView.setText("");
             myTextView2.setText("");
-            m1 = 0;
-            op1 = '+';
-        }
-        else if (v.getId() == R.id.buttonSuma) calcula ('+');
-        else if (v.getId() == R.id.buttonResta) calcula ('-');
-        else if (v.getId() == R.id.buttonMultiplicar) calcula ('*');
-        else if (v.getId() == R.id.buttonDivide) calcula ('/');
-        else if (v.getId() == R.id.buttonIgual) calcula ('=');
+            numero1 = 0;
+            operacion = '+';
+        } else if (v.getId() == R.id.buttonSuma) calcular('+');
+        else if (v.getId() == R.id.buttonResta) calcular('-');
+        else if (v.getId() == R.id.buttonMultiplicar) calcular('*');
+        else if (v.getId() == R.id.buttonDivide) calcular('/');
+        else if (v.getId() == R.id.buttonIgual) calcular('=');
     }
 
-    public void calcula (char op){
-        double result=m1;
-        String cadena= myTextView.getText().toString();
-        try{
-            m2=Double.parseDouble(cadena);
-            if(op1=='+') result=m1+m2;
-            else if (op1=='-') result=m1-m2;
-            else if (op1=='*') result=m1*m2;
-            else if (op1=='/') result=m1/m2;
-            m1=result;
-            op1=op;
-            if (op1=='='){
-               //myTextView.setText("");
-                myTextView2.setText(""+m1);
-            }else {
+    public void calcular(char a) {
+         double result=numero1;
+        String cadena = myTextView.getText().toString();
+        try {
+            numero2 = Double.parseDouble(cadena);
+            if (operacion == '+') result = numero1 + numero2;
+            else if (operacion == '-') result = numero1 - numero2;
+            else if (operacion == '*') result = numero1 * numero2;
+            else if (operacion == '/') result = numero1 / numero2;
+            numero1 = result;
+            operacion = a;
+            if (a == '=') {
+                //myTextView.setText("");
+                myTextView2.setText("" + numero1);
+            } else {
                 myTextView.setText("");
-                myTextView2.setText(""+m1);
+                myTextView2.setText("" + numero1);
             }
 
-        }catch (NumberFormatException nfe){
-            Toast.makeText(this,"numero incorrecto",1).show();
+        } catch (Exception nfe) {
+            myTextView.setText(" no es valida la operación");
         }
     }
-
 }
+
